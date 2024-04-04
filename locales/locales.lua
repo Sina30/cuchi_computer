4cabd8d7243e28a220910db6bed746aa0fde6039
Sina <santiano38@googlemail.com>
2024-04-04 14:28:43 +0200

    Update locales.lua

--- locales/locales.lua
+++ locales/locales.lua
@@ -12,5 +12,12 @@ Locales = {
         in_vehicle = "Les ordinateurs portables ne peuvent pas être utilisés dans un véhicule.",
         data_heist = "Vol de données",
         data_heist_call = "Vol de données en cours, GPS réglé sur la position localisée.",
+    },
+    DE = {
+        start_computer = "Drücke ~INPUT_CONTEXT~ um den Computer zu benutzen",
+        already_in_use = "Dieser Computer wird gerade benutzt",
+        in_vehicle = "Sie können den Laptop nicht in einem Fahrzeug benutzen",
+        data_heist = "Data Heist",
+        data_heist_call = "es läuft ein Data Heist, GPS wurde gesetzt.",
     }
 }
